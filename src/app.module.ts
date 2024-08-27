7ed053e82debdea2fa6ab20a29008022bf161172
pedromourax <pedrolucas.moura@hotmail.com>
2024-08-27 17:30:21 -0300

    config enviromente & mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CostumerSchema } from './interfaces/costumer.schema';
+import { enviroment } from './enviroment';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(enviroment.mongodb_uri),
+    MongooseModule.forFeature([{ name: 'Costumer', schema: CostumerSchema }]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
