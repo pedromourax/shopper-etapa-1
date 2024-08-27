9a6a06a1c7e8d97bcc9647b8954fad67a737693c
pedromourax <pedrolucas.moura@hotmail.com>
2024-08-27 17:29:41 -0300

    upload endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { UploadDto } from './dtos/upload.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello() {
     return this.appService.getHello();
+    // return this.appService.test(uploadBody);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async Upload(@Body() uploadBody: UploadDto) {
+    return this.appService.upload(uploadBody);
   }
 }
