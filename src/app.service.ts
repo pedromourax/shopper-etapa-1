afbadd557e531a720e160c6a831f5a4d70434a84
pedromourax <pedrolucas.moura@hotmail.com>
2024-08-27 17:30:41 -0300

    create upload service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { UploadDto } from './dtos/upload.dto';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const envtest = process.env.GEMINI_API_KEY;
+    console.log('env test', envtest);
+    return envtest;
+  }
+
+  async upload(uploadBody: UploadDto): Promise<any> {
+    const genIA = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+    const model = genIA.getGenerativeModel({ model: 'gemini-1.5-pro' });
+
+    const result = await model.generateContent([
+      {
+        inlineData: {
+          mimeType: 'image/jpeg',
+          data: uploadBody.image,
+        },
+      },
+      {
+        text: 'retorne apenas números de quanto marca esse hodômetro',
+      },
+    ]);
+    return result.response.text();
   }
 }
