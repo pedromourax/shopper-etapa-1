3cadd309ff1174ddd2f749e1aee81654ca0963cb
pedromourax <pedrolucas.moura@hotmail.com>
2024-08-27 17:30:57 -0300

    add db_uri

---
+++ src/enviroment.ts
@@ -0,0 +1,4 @@
+export const enviroment = {
+  mongodb_uri:
+    'mongodb+srv://teste-tecnico:YYeTydXhGqNGnjVU@cluster0.nf6to.mongodb.net/testetecnico?retryWrites=true&w=majority&appName=Cluster0',
+};
