ee779b0c1aceb0c12cfa716e30e3b7ff2cdf030f
pedromourax <pedrolucas.moura@hotmail.com>
2024-08-28 00:22:42 -0300

    add costumer_code

--- src/interfaces/costumer.interface.ts
+++ src/interfaces/costumer.interface.ts
@@ -3,5 +3,6 @@ import { Measure } from './measure.interface';
 
 export interface Costumer extends Document {
   readonly _id: string;
-  measure: Measure[];
+  costumer_code: string;
+  measures: Measure[];
 }
