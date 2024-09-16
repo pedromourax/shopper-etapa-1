9bafce7875b6a50c2a3c43878e624242796fc490
pedromourax <pedrolucas.moura@hotmail.com>
2024-09-16 16:29:48 -0300

    change cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { json, urlencoded } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(json({ limit: '50mb' }));
-  app.enableCors();
+  app.enableCors({
+    origin: "*",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(3000);
 }
